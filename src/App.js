3d35707e2187fd67e01717d933580be63f117f35
Meg <megunaokawa@gmail.com>
2019-06-05 03:19:07 +0900

    some files didn't get committed with last commit.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import './App.css';
-import RecipeList from './modules/RecipeList';
-import RecipeInfo from './modules/RecipeInfo';
+import RecipeIndex from './modules/RecipeIndex';
 
 function App() {
   return (
     <div className="app">
       <header className="app-main-header">In the Mood for Food</header>
-      <div className="app-recipe-wrapper">
-        <div className="app-recipelist"><RecipeList /></div>
-        <div className="app-recipelist"><RecipeInfo /></div>
-      </div>
-
+			<RecipeIndex />
     </div>
   );
 }
